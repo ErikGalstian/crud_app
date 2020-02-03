fbe523a76f36b393bc1778c799dbc8e6376a45a2
Erik Galstian <ErikGalstian94@gmail.com>
2020-02-03 22:46:13 +0400

    change the for loop to foreach

---
+++ home.js
@@ -0,0 +1,69 @@
+// Function that adds a button
+const buttonAdd = () => {
+  // Checking if the input is empty
+  if (!document.querySelector('#input').value) {
+    alert('Title field must not be empty!');
+    return;
+  }
+  const item = {
+    id: counter,
+    text: document.querySelector('#input').value
+  };
+  // POST - add an item
+  fetch('http://localhost:4000/api/todos', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  });
+  document.querySelector('#input').value = ''; // <--- Reset the input field after adding the item
+  fetchTable();
+};
+
+// Function that deletes an item
+const buttonDelete = id => {
+  const item = {
+    id: id,
+    text: document.querySelector('#input').value
+  };
+  // DELETE - delete an item
+  fetch('http://localhost:4000/api/todos', {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  });
+  fetchTable();
+};
+
+// Fetch function that fetches and renderes the table
+const fetchTable = () => {
+  document.querySelector('#table').innerHTML = '';
+  fetch('http://localhost:4000/api/todos')
+    .then(response => response.json())
+    .then(data => {
+      const items = data[0];
+      counter = data[1];
+      console.log(items);
+      items.forEach(item => {
+        const newRow = `
+            <tr class="item" id=${item.id}>
+                <td>${item.text}</td>
+                <td>
+                    <form action="/edit" method="post">
+                        <button type="submit" name="id" value="${item.id}">Edit</button>
+                        <button type="button" onclick="buttonDelete(${item.id})">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        `;
+        document.querySelector('#table').innerHTML += newRow;
+      });
+    });
+};
+
+fetchTable();
